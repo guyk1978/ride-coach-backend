aaa7a170a1d2a02dc1104d95e96efb3893805594
guyk1978 <99318454+guyk1978@users.noreply.github.com>
2025-08-10 22:33:33 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'srv535.hstgr.io',      // או 185.28.21.52
-  user: 'u220582403',
+  user: 'u220582403_ebikeroutes_db',
   password: 'Kesem111@',          // הסיסמה החדשה
-  database: 'ebikeroutes_db',
+  database: 'u220582403',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
